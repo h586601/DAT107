94d177a21c2779a645be9e9eb29f264210fd855a
Lars-Petter Helland <lars-petter.helland@hvl.no>
2020-03-25 12:57:54 +0100

    F2 eksempel2

---
+++ F2_Eksempel2_Surrogate_Key/src/no/hvl/dat107/Main2.java
@@ -0,0 +1,16 @@
+package no.hvl.dat107;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+
+        PersonDAO personDAO = new PersonDAO();
+
+        Person lars = new Person();
+        lars.setNavn("Lars");
+        System.out.println(lars);
+        personDAO.savePerson(lars);
+        System.out.println(lars);
+    }
+
+}
